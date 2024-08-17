70b418f4508bf4d579b75d914d48b02216116d45
Anshika Verma <anshikaverma2411@gmail.com>
2024-08-17 12:46:33 +0530

    encrypted password and creted jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -11,10 +13,13 @@ const createUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
   if (userExists) res.status(400).send("user already exists");
 
-  const newUser = new User({ username, email, password });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
+    createToken(res, newUser._id);
 
     res.status(201).json({
       _id: newUser._id,
