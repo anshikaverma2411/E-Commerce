7ffa25a0b004208920beb431caf8bf0e64f6b453
Anshika Verma <anshikaverma2411@gmail.com>
2024-09-03 00:01:39 +0530

    Ui for register form

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import { useRegisterMutation } from "../../redux/api/usersApiSlice";
+const Register = () => {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  return (
+    <section className="pl-[10rem] flex flex-wrap">
+      {" "}
+      <div className="mr-[4rem] mt-[5rem]">
+        <h1 className="text-2xl font-semibold mb-4">Register</h1>
+
+        <form className="conatiner w-[40rem]">
+          <div className="my-[2rem]">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-black"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter name"
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-black"
+            >
+              Email Address
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-black"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-black"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="Password"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-[1rem] "
+          >
+            {isLoading ? "Registering..." : "Register"}
+          </button>
+          {isLoading && <Loader />}
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
